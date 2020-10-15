d12b20e5ee226f1f42e287fa0b203aa0af90a640
FILIPE ROSA DA SILVA <filipe331@gmail.com>
2020-10-15 11:48:33 -0300

    Teste de pesquisa do Google

--- StarWarsApi/StarWarsApi/Program.cs
+++ StarWarsApi/StarWarsApi/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RestSharp;
 using StarWarsApi.DataBase;
+using System.IO;
+using System.Net;
 
 namespace StarWarsApi
 {
@@ -13,15 +15,31 @@ namespace StarWarsApi
     {
         static void Main(string[] args)
         {
+            WebClient client2 = new WebClient();
             var client = new RestClient("https://swapi.dev/api/planets/");
             var request = new RestRequest(Method.GET);
             var response = client.Execute(request);
 
-            var planetas = JsonConvert.DeserializeObject <PlanetasModel> (response.Content);
+            var planetas = JsonConvert.DeserializeObject<PlanetasModel>(response.Content);
 
             foreach (var p in planetas.results)
             {
                 Console.WriteLine($"{p.name} rotation {p.diameter}");
+
+                /*Console.WriteLine("Qual a Pesquisa?");
+                //var query = Console.ReadLine();
+                var endereco = ("https://www.google.com.br/search?q=" + $"{p.name}");
+                //var endereco = ("https://www.google.com.br/search?q=" + query);
+                //Process.Start("https://www.google.com.br/search?q=" + query);
+
+                var recebe = client2.DownloadString(endereco);
+                Console.WriteLine(recebe);
+
+                string nomeArquivo = @"C:\GitHub\TesteVS2017\log - " + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+                StreamWriter writer = new StreamWriter(nomeArquivo);
+                writer.WriteLine(recebe);
+                
+                writer.Close();*/
             }
 
             Console.ReadLine();
