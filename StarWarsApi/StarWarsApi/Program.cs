ba137dedb5e4c08b10e5e401f3156c8bdb59afad
FILIPE ROSA DA SILVA <filipe331@gmail.com>
2020-10-15 11:24:57 -0300

    Ajuste no DeserializeObject

--- StarWarsApi/StarWarsApi/Program.cs
+++ StarWarsApi/StarWarsApi/Program.cs
@@ -13,16 +13,18 @@ namespace StarWarsApi
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("https://swapi.co/api/planets/");
+            var client = new RestClient("https://swapi.dev/api/planets/");
             var request = new RestRequest(Method.GET);
             var response = client.Execute(request);
 
-            var planetas = JsonConvert.DeserializeObject < (new { results = new List<PlanetModel>() }))> (response.Content);
+            var planetas = JsonConvert.DeserializeObject <PlanetasModel> (response.Content);
 
             foreach (var p in planetas.results)
             {
-                System.Console.WriteLine($"{p.name} rotation {p.rotation_period}");
+                Console.WriteLine($"{p.name} rotation {p.diameter}");
             }
+
+            Console.ReadLine();
         }
     }
 }
